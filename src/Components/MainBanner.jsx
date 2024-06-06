2b30674b8bb376086dba9523a37ae6234700f6fc
Hari K <hari@LH1808265088.local>
2024-06-06 14:06:32 +0100

    main

--- src/Components/MainBanner.jsx
+++ src/Components/MainBanner.jsx
@@ -1,11 +1,12 @@
-import bannerImage from '../assets/banner-image.jpg'; // Add your image to the assets folder
-import { useState } from 'react';
-import LoginForm from './/LoginForm.jsx';
+import { useState, useEffect, useRef } from 'react';
+import LoginForm from './LoginForm.jsx';
+import { page_desc, page_title, bannerVideo } from '../Utils/constants.js';
 
 const MainBanner = () => {
   const [showLogin, setShowLogin] = useState(false);
+  const videoRef = useRef(null);
 
-  const handlePlayVideoClick = () => {
+  const handleLoginForm = () => {
     setShowLogin(true);
   };
 
@@ -13,18 +14,41 @@ const MainBanner = () => {
     setShowLogin(false);
   };
 
+  useEffect(() => {
+    if (videoRef.current) {
+      const video = videoRef.current;
+
+      const handleTimeUpdate = () => {
+        if (video.currentTime >= 6) {
+          video.currentTime = 0;
+          video.play();
+        }
+      };
+
+      video.addEventListener('timeupdate', handleTimeUpdate);
+
+      // Cleanup the event listener if the component unmounts
+      return () => {
+        video.removeEventListener('timeupdate', handleTimeUpdate);
+      };
+    }
+  }, [videoRef]);
+
   return (
-    <div className="relative h-screen bg-cover bg-center" style={{ backgroundImage: `url(${bannerImage})` }}>
+    <div className="relative h-screen">
+      <video
+        ref={videoRef}
+        className="absolute inset-0 w-full h-full object-cover"
+        src={bannerVideo}
+        autoPlay
+        muted
+      ></video>
       <div className={`absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center ${showLogin ? 'hidden' : 'flex'}`}>
         <div className="text-center text-white">
-          <h1 className="text-8xl font-cursive">Welcome to Dance Mate !</h1>
-          <h2 className="text-4xl mt-3 pl-10 pr-10">
-            The Dance Mate App stands out as an innovative online platform committed to elevating the dance education journey.
-            Prioritizing accessibility, interactivity, and community development, this web application transforms the landscape of dance instruction,
-            serving the needs of both dance enthusiasts and professionals!
-          </h2>
+          <h1 className="text-8xl font-cursive">{page_title}</h1>
+          <h2 className="text-4xl mt-3 pl-10 pr-10"> {page_desc} </h2>
           <button
-            onClick={handlePlayVideoClick}
+            onClick={handleLoginForm}
             className="mt-5 px-6 py-3 text-lg bg-white text-black hover:bg-gray-200"
           >
             Get Started
---
+++ src/Utils/constants.js
@@ -0,0 +1,7 @@
+export const page_title = "Welcome to Dancemate!"
+
+export const page_desc = `The Dance Mate App is an online platform dedicated to enhancing the dance education experience.
+                            By emphasizing accessibility, interactivity, and community building, this web application revolutionizes dance instruction,
+                            catering to the needs of both dance enthusiasts and professionals.`
+
+export const bannerVideo = "https://videos.pexels.com/video-files/2795731/2795731-uhd_3840_2160_25fps.mp4"
